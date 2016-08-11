a8aaab7427537dd59877dbb20449589c56a7a2a5
Dominggus Salampessy <dominggus@e-sites.nl>
2016-08-11 11:40:50 +0200

    bugfix: re-enable panGesture

--- DrawerController/DrawerController.swift
+++ DrawerController/DrawerController.swift
@@ -1153,7 +1153,7 @@ public class DrawerController: UIViewController, UIGestureRecognizerDelegate {
                     self.gestureCompletionBlock!(self, panGesture)
                 }
             })
-            
+            panGesture.enabled = true
             self.view.userInteractionEnabled = true
         default:
             break
