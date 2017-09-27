416b2d6c2a50e0ce9454cb678841a6b0ee146755
zeDDer <criticalexception@gmail.com>
2017-09-28 01:00:34 +0300

    Swift 4 support + project settings for Xcode9

--- DrawerController/DrawerController.swift
+++ DrawerController/DrawerController.swift
@@ -1108,7 +1108,7 @@ open class DrawerController: UIViewController, UIGestureRecognizerDelegate {
     
     // MARK: - Gesture Handlers
     
-    func tapGestureCallback(_ tapGesture: UITapGestureRecognizer) {
+    @objc func tapGestureCallback(_ tapGesture: UITapGestureRecognizer) {
         if self.openSide != .none && self.animatingDrawer == false {
             self.closeDrawer(animated: true, completion: { (finished) in
                 if self.gestureCompletionBlock != nil {
@@ -1118,7 +1118,7 @@ open class DrawerController: UIViewController, UIGestureRecognizerDelegate {
         }
     }
     
-    func panGestureCallback(_ panGesture: UIPanGestureRecognizer) {
+    @objc func panGestureCallback(_ panGesture: UIPanGestureRecognizer) {
         switch panGesture.state {
         case .began:
             if self.animatingDrawer {
